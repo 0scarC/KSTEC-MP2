5d75efe33d6ee99b3bcee45a718e2d927fe8e823
Admin <brycealvir@gmail.com>
2022-08-16 15:53:56 -0700

    building backend

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,62 +1,16 @@
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
-
 import './App.css';
 
-import Home from './components/Home'
-import About from './components/About'
-import Workouts from './components/Workouts'
-import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
-
-
-
 function App() {
-  
-  const workouts = ['Pushups', 'Pullups' ]
-
   return (
     <div className="App">
-      <Router>
-        <header>
-          <h1 className="title">Workout Tracker</h1>
+      <header className="App-header">
+        <p>
+          Testing
+        </p>
 
-          <Container>
-    <Nav defaultActiveKey="/" variant="tabs" fill>
-        <Nav.Item>
-            <Nav.Link href="/"> 
-                <Link to="/">Home</Link>
-            </Nav.Link>
-        </Nav.Item>
-        <Nav.Item >
-            <Nav.Link eventKey={"aboutPage"}> 
-                <Link to="/about">About Us</Link>
-            </Nav.Link>
-        </Nav.Item>
-        <Nav.Item >
-            <Nav.Link eventKey={"workoutsPage"}> 
-                <Link to="/workouts">All Workouts</Link> 
-            </Nav.Link>
-         </Nav.Item>
-    </Nav>
-</Container>
-
-        </header>
-
-        <div className="display">
-          <Route path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/workouts" render={() => <Workouts workouts={workouts}/>}  />
-        </div>
-
-      </Router>
-
+      </header>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-

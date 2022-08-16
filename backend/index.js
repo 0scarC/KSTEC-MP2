07a00d625ae4fdd114e2005d72e904977af86ace
Admin <brycealvir@gmail.com>
2022-08-16 16:30:18 -0700

    successfully connected to mongo

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 
 require('dotenv').config();
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +12,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+  });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+

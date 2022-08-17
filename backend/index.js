60c08bbb397273317417df7e4d31dc55a9508b8e
Admin <brycealvir@gmail.com>
2022-08-16 17:17:41 -0700

    finished exercise backend routes

--- backend/index.js
+++ backend/index.js
@@ -17,12 +17,17 @@ mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true
   });
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
-
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
-
+  const connection = mongoose.connection;
+  connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+  })
+  
+  const exercisesRouter = require('./routes/exercises');
+  const usersRouter = require('./routes/users');
+  
+  app.use('/exercises', exercisesRouter);
+  app.use('/users', usersRouter);
+  
+  app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+  });
